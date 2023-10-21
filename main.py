8d6944eac06b3d84cd1c9e5be973b5d397eb95fd
Shkam1K <d.shkamat@gmail.com>
2023-10-21 20:12:48 +0300

    new

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import random
+import pygame
+
 suits = ['♠', '♣', '♦', '♥']
 ranks = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
 deck = {rank + suit: [ranks.index(rank) + 6, suit] for rank in ranks for suit in suits}
-
 list_deck = list(deck)
 
-def trump():
-    pass
 
+def trump(suits: list):
+    suit = random.sample(suits)
+    return suit
+
+    
 def hand(deck: list):
     random_elements = random.sample(range(len(deck)), 6)
     hand = [deck[index] for index in random_elements]
     # deck = [element for index, element in enumerate(deck) if index not in hand] 
     return hand
 
-def heap(hand):
+def heap(hand: list):
     pass
 
-def playing_deck():
-    pass
+def table(hand1: list, hand2: list):
+    table = []
+    
+    return table
+
 
 print(list_deck)
--- start.py
+++ start.py
@@ -1,27 +1,52 @@
-import sys
-import main
 import pygame
-
+import sys
+import random
+from PIL import Image
+# Инициализация Pygame
 pygame.init()
-WINDOW_SIZE = (1780, 720)
+
+# Установка размеров окна
+WINDOW_SIZE = (800, 600)
 window = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption('Дурак')
+pygame.display.set_caption('Игра в Дурака')
+
+# Цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 # Шрифт для текста
 font = pygame.font.Font(None, 36)
+
+# Основной цикл игры
 while True:
-    # Обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()  # Выход из игры
+            pygame.quit()
             sys.exit()
 
-    # Отрисовка объектов и графики
-    window.fill((255, 255, 255))  # Заливка окна белым цветом
+    # Отрисовка экрана
+    window.fill(WHITE)
+    
+    # Отображение текста и графики
+    text = font.render('Игра в Дурака', True, BLACK)
+    window.blit(text, (300, 50))
 
-    # Здесь вы можете добавить код для отрисовки других объектов или графики
+    # Отображение карт игрока и компьютера (заглушка)
+    # Замените этот блок кода на вашу логику отображения карт
+    player_hand = ['2', '3', '4', '5', '6']
+    for i, card in enumerate(player_hand):
+        card_text = font.render(card, True, RED)
+        window.blit(card_text, (100 + i * 80, 300))
 
-    pygame.display.update()  # Обновление экрана
+    computer_hand = ['7', '8', '9', '10', 'J']
+    for i, card in enumerate(computer_hand):
+        card_text = font.render(card, True, RED)
+        window.blit(card_text, (100 + i * 80, 100))
+
+    # Отображение кнопки для хода (заглушка)
+    pygame.draw.rect(window, BLACK , (350, 450, 100, 50))
+    button_text = font.render('Ход', True, WHITE)
+    window.blit(button_text, (370, 460))
+
+    pygame.display.update()
